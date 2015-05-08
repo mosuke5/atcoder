b66a24243a1dc102f508f02061c98824514a67a2
mosuke5 <g.vamos603@gmail.com>
2015-05-08 21:53:22 +0900

    first commit

---
+++ abc022_a/main.rb
@@ -0,0 +1,23 @@
+# http://abc022.contest.atcoder.jp/tasks/abc022_a
+
+#
+## 標準入力受け取り
+#
+N,S,T=gets.chomp.split(" ").map(&:to_i)
+W = gets.to_i
+A  = []
+(N-1).times do 
+  A.push(gets.to_i)
+end
+
+#
+## 理想体重計算
+#
+w = W
+result = 0
+A.each do |a|
+  w += a
+  result+=1 if w > S && w < T
+end
+
+puts result
