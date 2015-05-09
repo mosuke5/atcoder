21cc545546744e91dabfba9fa759dbc967bd3cb8
mosuke5 <g.vamos603@gmail.com>
2015-05-09 19:28:19 +0900

    変数名をわかりやすく修正

--- abc022_a/main.rb
+++ abc022_a/main.rb
@@ -2,22 +2,27 @@
 
 #
 ## 標準入力受け取り
+## n: 日数
+## min: 理想体重の最低値
+## max: 理想体重の最大値
+## weight: 初期の体重
+## change: 体重の変動値 ex) [10,20,-10,5]
 #
-N,S,T=gets.chomp.split(" ").map(&:to_i)
-W = gets.to_i
-A  = []
-(N-1).times do 
-  A.push(gets.to_i)
+n,min,max=gets.chomp.split(" ").map(&:to_i)
+weight = gets.to_i
+change  = []
+(n-1).times do 
+  change.push(gets.to_i)
 end
 
 #
 ## 理想体重計算
 #
-w = W
+tmp_weight = weight
 result = 0
-A.each do |a|
-  w += a
-  result+=1 if w >= S && w <= T
+change.each do |a|
+  tmp_weight += a
+  result+=1 if tmp_weight >= min && tmp_weight <= max
 end
 
 puts result
